505473f26fb9caf5964f07f998a8c5b6f21709e0
PkSM3 <pokesam3@gmail.com>
2013-12-02 10:40:01 -0300

    using the dbs received in params_details.php

--- php/parameters_details.php
+++ php/parameters_details.php
@@ -1,7 +1,17 @@
 <?php
 // Common parameters for all function proposing insight into the corpora
-$mainpath=dirname(dirname(getcwd()))."/data/"; // -> /var/www/ademe/data/
-$dbname=getDB($mainpath);//'homework-20750-1-homework-db.db';
+//$mainpath=dirname(dirname(getcwd()))."/data/"; // -> /var/www/ademe/data/
+//$dbname=getDB($mainpath);//'homework-20750-1-homework-db.db';
+//$base = new PDO("sqlite:" .$mainpath.$dbname);
+//$max_item_displayed=6;
+
+$dbs= $_GET["dbs"];//I receive the databases as json!
+$dbs = json_decode($dbs);
+var_dump($dbs);
+
+$mainpath=dirname(dirname(getcwd()))."/"; // -> /var/www/ademe/data/pathtodatabase/and.db
+$dbname=$dbs[0];// data/pathtodb/the.db
+////getDB($mainpath);//'homework-20750-1-homework-db.db';
 $base = new PDO("sqlite:" .$mainpath.$dbname);
 $max_item_displayed=6;
 
