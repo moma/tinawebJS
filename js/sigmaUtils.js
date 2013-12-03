81f5666387ce957745d9e73885dc174e9235df39
PkSM3 <pokesam3@gmail.com>
2013-12-03 18:46:48 -0300

    utils

---
+++ js/sigmaUtils.js
@@ -0,0 +1,16 @@
+
+function prNodes(){
+    pr(partialGraph._core.graph.nodes);
+}
+
+function prNodesIndex(){
+    pr(partialGraph._core.graph.nodesIndex);
+}
+
+function prEdges(){
+    pr(partialGraph._core.graph.edges);
+}
+
+function prEdgesIndex(){
+    pr(partialGraph._core.graph.edgesIndex);
+}
