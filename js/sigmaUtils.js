1be5783c8daf807511a5a20721e54b4a53201644
PkSM3 <pokesam3@gmail.com>
2013-12-04 08:25:15 -0300

    utils update

--- js/sigmaUtils.js
+++ js/sigmaUtils.js
@@ -1,16 +1,51 @@
 
-function prNodes(){
-    pr(partialGraph._core.graph.nodes);
+function getnodes(){
+    return partialGraph._core.graph.nodes;
 }
 
-function prNodesIndex(){
-    pr(partialGraph._core.graph.nodesIndex);
+function getnodesIndex(){
+    return partialGraph._core.graph.nodesIndex;
 }
 
-function prEdges(){
-    pr(partialGraph._core.graph.edges);
+function getedges(){
+    return partialGraph._core.graph.edges;
 }
 
-function prEdgesIndex(){
-    pr(partialGraph._core.graph.edgesIndex);
+function getedgesIndex(){
+    return partialGraph._core.graph.edgesIndex;
+}
+
+function getn(id){
+    return partialGraph._core.graph.nodesIndex[id];
+}
+
+function gete(id){
+    return partialGraph._core.graph.edgesIndex[id];
+}
+
+function find(label){
+    results=[];
+    nds=getnodesIndex();
+    for(var i in nds){
+        n=nds[i];
+        if(n.hidden==false){
+            if (n.label.indexOf(label)!==-1) {
+                results.push(n);
+            }  
+        }
+    }
+    return results;
+}
+
+function exactfind(label) {
+    nds=getnodesIndex();
+    for(var i in nds){
+        n=nds[i];
+        if(n.hidden==false){
+            if (n.label==label) {
+                return n;
+            }
+        }
+    }
+    return null;
 }
