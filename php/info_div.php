3a15df979d8e7fa58ff9ef1896bc32d4be5b65e1
David Chavalarias <david.chavalarias@polytechnique.edu>
2013-12-07 23:04:37 +0100

    ademe exception removed in infodov

--- php/info_div.php
+++ php/info_div.php
@@ -5,9 +5,9 @@ include('parameters_details.php');
 // info_div should extract the info from *data.db
 // if the rock proj has this in rule, we'll remove this "exception"
 $isAdeme=$_SERVER["SCRIPT_FILENAME"];
-if (strpos($isAdeme, 'ademe') !== false) $base = new PDO("sqlite:" .$mainpath.$datadb);
-else $base = new PDO("sqlite:" .$mainpath.$graphdb);
-
+//if (strpos($isAdeme, 'ademe') !== false) $base = new PDO("sqlite:" .$mainpath.$datadb);
+//else $base = new PDO("sqlite:" .$mainpath.$graphdb);
+$base = new PDO("sqlite:" .$mainpath.$graphdb);
 
 include('default_div.php');
 ?>
