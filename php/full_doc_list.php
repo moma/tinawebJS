da4067c7617cd694f56d579801589e36695fa837
David Chavalarias <david.chavalarias@polytechnique.edu>
2013-12-16 20:19:37 +0100

    full doc details with tfidf

--- php/full_doc_list.php
+++ php/full_doc_list.php
@@ -8,6 +8,7 @@ $gexf='2013.gexf';//$_GET["gexf"];
 $base = new PDO("sqlite:" .$mainpath.$db);
 
 $temp=explode('/',$db);
+$project_folder=$temp[1];
 $corpora=$temp[count($temp)-2];
 $corporadb = new PDO("sqlite:" .$mainpath.'data/'.$corpora.'/'.$corpora.'.sqlite'); //data base with complementary data
 
@@ -26,13 +27,9 @@ foreach ($base->query($sql) as $row) {
   $table_size=$row['COUNT(*)'];
 }
 
-
 ///// Specific to rock //////////
 // Other restrictions
 // extracting the project folder and the year
-$temp=explode('/',$thedb);
-$project_folder=$temp[1]; 
-//echo $gexf;
 if (strpos($gexf,'2013')>0){
   $year='2013'; 
   $year_filter=true;
@@ -113,7 +110,7 @@ foreach ($corporadb ->query($sql) as $row) {
 $number_doc=ceil(count($wos_ids)/3);
 $count=0;
 foreach ($wos_ids as $id => $score) { 
-  if ($count<$max_item_displayed){
+  if ($count<1000){
     // retrieve publication year
     $sql = 'SELECT data FROM ISIpubdate WHERE id='.$id;
     foreach ($base->query($sql) as $row) {
@@ -143,15 +140,15 @@ foreach ($wos_ids as $id => $score) {
       $sql = 'SELECT data FROM ISITITLE WHERE id='.$id;
 
       foreach ($base->query($sql) as $row) {
-        $output.='<a href="JavaScript:newPopup(\''.$twjs.'php/default_doc_details.php?db='.urlencode($thedb).'&query='.urlencode($query).'&type='.urlencode($_GET["type"]).'&id='.$id.' \')">'.$row['data']." </a> ";
+         $output.='<a href="default_doc_details.php?db='.urlencode($db).'&type='.urlencode($_GET["type"]).'&query='.urlencode($query).'&id='.$id.'">'.$row['data']." </a> ";
 
                         //this should be the command:
       //$output.='<a href="JavaScript:newPopup(\''.$twjs.'php/default_doc_details.php?db='.urlencode($datadb).'&id='.$id.'  \')">'.$row['data']." </a> "; 
 
                         //the old one:  
       //$output.='<a href="JavaScript:newPopup(\''.$twjs.'php/default_doc_details.php?id='.$id.'  \')">'.$row['data']." </a> ";   
-        $external_link="<a href=http://scholar.google.com/scholar?q=".urlencode('"'.$row['data'].'"')." target=blank>".' <img width=8% src="'.$twjs.'img/gs.png"></a>'; 
-    //$output.='<a href="JavaScript:newPopup(''php/doc_details.php?id='.$id.''')"> Link</a>'; 
+        $external_link="<a href=http://scholar.google.com/scholar?q=".urlencode('"'.$row['data'].'"')." target=blank>".' <img width=20px src="img/gs.png"></a>'; 
+        //$output.='<a href="JavaScript:newPopup(''php/doc_details.php?id='.$id.''')"> Link</a>'; 
       }
 
   // get the authors
@@ -179,7 +176,12 @@ foreach ($wos_ids as $id => $score) {
     continue;
   }
 }
-$output .= "</ul>[".$count." top items]"; #####
+
+$output= '<h3>'.$count.' items related to: '.implode(' OR ', $elems).'</h3>'.$output;
+
+
+
+echo $output;
 
 
 function imagestar($score,$factor,$twjs) {
@@ -188,15 +190,14 @@ function imagestar($score,$factor,$twjs) {
   if ($score > .5) {
     $star_image = '';
     for ($s = 0; $s < min(5,$score/$factor); $s++) {
-      $star_image.='<img src="'.$twjs.'img/star.gif" border="0" >';
+      $star_image.='<img src="img/star.gif" border="0" >';
     }
   } else {
-    $star_image.='<img src="'.$twjs.'img/stargrey.gif" border="0">';
+    $star_image.='<img src="img/stargrey.gif" border="0">';
   }
   return $star_image;
 }
 
-echo $output;
 ?>
 
 
